2f087402e8cb08159f7548367406eca46f0c871c
PodolinaMaria <muukmain@gmail.com>
2025-12-03 14:54:43 +0300

    Added user service

---
+++ PatientAccounting/Services/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatientsAcounting.Services
+{
+    public static class CurrentUser
+    {
+        public static int UserId { get; set; }
+        public static string Username { get; set; }
+        public static int RoleId { get; set; }
+        public static int? PatientId { get; set; }
+        public static bool IsAuthenticated => UserId > 0;
+
+        public static void Clear()
+        {
+            UserId = 0;
+            Username = string.Empty;
+            RoleId = 0;
+            PatientId = null;
+        }
+    }
+}
